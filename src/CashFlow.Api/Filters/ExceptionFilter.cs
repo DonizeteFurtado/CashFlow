1819c6e7fe38eb77f580a3945a0bc3fad7551100
Carlos Furtado <fonizetefurtado24@gmail.com>
2025-10-01 23:38:56 -0300

    Realizado ajuste para avançar com o desenvolvimento da api

---
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,49 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException)
+        {
+            HandleProjetctException(context);
+        }
+        else
+        {
+            ThrowUnkowError(context);
+        }
+    }
+
+    private void HandleProjetctException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var exc = (ErrorOnValidationException)context.Exception;
+
+            var errorResponse = new ResponseErrorJson(exc.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+        else
+        {
+            var errorResponse = new ResponseErrorJson(context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+
+    private void ThrowUnkowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson("unknown error");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
---
+++ src/CashFlow.Applicantion/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validate = new RegisterExpenseValidator();
+
+        var result = validate.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
